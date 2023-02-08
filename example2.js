43b7fa08bd82ffddb8e2c0fab812f1bcbc71e2d0
Jinamizi <tonnylango@gmail.com>
2023-02-08 19:40:09 +0300

    some commit

---
+++ example2.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#example p").hover(function(){
+		$(this).css("background-color","#90EE90");
+	},
+	function(){
+		$(this).css("background-color","white");
+	});
+	
+	/*$("#example p").focus(function(){
+		$(this).css("background-color","#90EE90");
+	});*/
+
+	
+});
