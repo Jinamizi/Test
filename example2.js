2f58cc75d7b1530c3789a48ef88c4e9da6b24ab6
Jinamizi <tonnylango@gmail.com>
2023-06-18 11:32:53 +0300

    updated

--- example2.js
+++ example2.js
@@ -10,5 +10,38 @@ $(document).ready(function(){
 		$(this).css("background-color","#90EE90");
 	});*/
 
+
+  if ('wakeLock' in navigator) {
+    $('#example').hover(function() {
+      // Request a screen wake lock
+      navigator.wakeLock.request('screen')
+        .then(function(wakeLock) {
+          // The wake lock has been successfully acquired
+          console.log('Screen wake lock activated!');
+
+          // Add an event listener to release the wake lock
+          wakeLock.addEventListener('release', function() {
+            console.log('Screen wake lock released!');
+          });
+        })
+        .catch(function(error) {
+          // Failed to acquire the wake lock
+          console.error('Failed to activate screen wake lock:', error);
+        });
+    });
+  } else {
+    // Wake Lock API is not supported
+    console.warn('Screen wake lock is not supported by this browser.');
+  }
+
+  
+  if ('wakeLock' in navigator) {
+    // Request a screen wake lock
+    navigator.wakeLock.request('screen')
+      
+}
+
+
+
 	
 });
